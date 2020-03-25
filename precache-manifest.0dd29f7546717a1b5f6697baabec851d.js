f119d8bbfb278cbd819e9898b9072e20f63f03ce
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-25 22:02:26 +0200

    Updates

---
+++ precache-manifest.0dd29f7546717a1b5f6697baabec851d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fce5baeaba7c493322e886535eb5fb",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "1b9e19180cbab1005980",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.ad091c06.chunk.css"
+  },
+  {
+    "revision": "8248ee56795ef0e34f19",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.26d4178d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.26d4178d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9e19180cbab1005980",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.6f6b6d44.chunk.js"
+  },
+  {
+    "revision": "a291afb40b779f416773",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.be5f401f.js"
+  }
+]);
